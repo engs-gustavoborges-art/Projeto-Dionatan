255352e8f2b19b58ec0f9ba8598deeefe1a2e1d7
engs-gustavosvartz <engs-gustavosvartz@camporeal.edu.br>
2026-03-25 23:48:38 -0300

    js carros

---
+++ js/comparar.js
@@ -0,0 +1,38 @@
+const carros = {
+    ferrari: {
+        nome: "Ferrari SF90",
+        velocidade: "340 km/h",
+        aceleracao: "2.5s",
+        preco: "R$ 4 milhões"
+    },
+    lambo: {
+        nome: "Lamborghini Aventador",
+        velocidade: "350 km/h",
+        aceleracao: "2.9s",
+        preco: "R$ 3 milhões"
+    },
+    onix: {
+        nome: "Onix",
+        velocidade: "180 km/h",
+        aceleracao: "10s",
+        preco: "R$ 80 mil"
+    },
+    civic: {
+        nome: "Honda Civic",
+        velocidade: "200 km/h",
+        aceleracao: "8s",
+        preco: "R$ 130 mil"
+    },
+    corolla: {
+        nome: "Toyota Corolla",
+        velocidade: "190 km/h",
+        aceleracao: "9s",
+        preco: "R$ 140 mil"
+    },
+    bmw: {
+        nome: "BMW M3",
+        velocidade: "290 km/h",
+        aceleracao: "3.9s",
+        preco: "R$ 600 mil"
+    }
+};
